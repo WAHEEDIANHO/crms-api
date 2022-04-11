19279fac5231f2daf7437b06a6413c4c0519532f
WAHEEDIANHO <waheedianho65@gmail.com>
2022-04-11 15:36:39 +0100

    updated version

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ const PORT = process.env.PORT || 7700;
 
 // Middlewares
 app.use(cors());
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
