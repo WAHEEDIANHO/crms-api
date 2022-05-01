b25d1b41c51d207018e45e3119da3796245b6831
WAHEEDIANHO <waheedianho65@gmail.com>
2022-05-01 14:51:59 +0100

    cors issue

--- app.js
+++ app.js
@@ -14,11 +14,15 @@ const app = express();
 const PORT = process.env.PORT || 7700;
 
 // Middlewares
+app.options("*", cors());
 app.use(cors());
 app.use(function (req, res, next) {
   //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
